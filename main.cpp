dc5cf72a012b59a00586970b7064a1d1f403a825
Yeetooq <roma20040402@gmail.com>
2024-10-09 21:55:06 +0300

    fixed namespaces

--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 using namespace std;
 int main() {
+69
  char name[20];
- std::cout << "Hello world! Have a nice day!\n";
+ cout << "Hello world! Have a nice day!\n";
  cout << "Enter your name: ";
  cin >> name;
  cout << "Hello, " << name << "!\n";
