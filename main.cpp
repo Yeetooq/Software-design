ea3163adc66e79c8d9e7925bf8dce83519b2e35b
Yeetooq <roma20040402@gmail.com>
2024-10-09 20:40:00 +0300

    added having a nice day wish

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+using namespace std;
 int main() {
- std::cout << "Hello world!\n";
+ char name[20];
+ cout << "Enter your name: ";
+ cin >> name;
+ cout << "Hello, " << name << "!\n";
+ cout << "I wish you a good day\n";
  system("pause");
  return 0;
-}
+} 
